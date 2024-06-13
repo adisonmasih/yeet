436ffe1b0003e7a1d9df6d3e0e262f151fee172c
Adison Masih <adicodetech@gmail.com>
2024-06-13 18:40:48 +0530

    update

--- commands/fun/dare.js
+++ commands/fun/dare.js
@@ -1,34 +1,35 @@
-const { SlashCommandBuilder, EmbedBuilder, Embed } = require('discord.js')
+const { SlashCommandBuilder, EmbedBuilder, Embed } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('dare')
-        .setDescription('Who Doesn\'t Like Dares? Every Does! (unless they have to do it)')
-        .addUserOption(option => option.setName('target').setDescription('the target user')),
+        .setName("dare")
+        .setDescription("Yeet Bot ❌ | Ass Kicking Bot ✅")
+        .addUserOption((option) =>
+            option.setName("target").setDescription("the target user")
+        ).addStringOption(
+            option => option.setName("rating").setDescription("The rating of the dare").addChoices({
+                name: "PG",
+                value: "pg"
+            }, {
+                name: "PG-13",
+                value: "pg13"
+            }, {
+                name: "R",
+                value: "r"
+
+            })
+        ),
     testOnly: true,
     async execute(interaction, client) {
-        const user = interaction.options.getUser('target') || interaction.user
-        const message = ``
-        //let res = await fetch("https://truth-dare.tk/api/dare/")
-        //let resJSON = await res.json()
-        //let truth = resJSON.dare
-	var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+        const user = interaction.options.getUser("target") || interaction.user;
+        const rating = interaction.options.getString("rating") || "pg";
+        const message = ``;
 
-var urlencoded = new URLSearchParams();
-urlencoded.append("category", "dirty");
-urlencoded.append("type", "dare");
 
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: urlencoded,
-  redirect: 'follow'
-};
+        let res = await fetch(`https://api.truthordarebot.xyz/v1/dare?rating=${rating}`)
+        let resJSON = await res.json();
+        let truth = resJSON?.question ?? "Something went wrong? try this command again";
 
-let res = await fetch("https://randommer.io/truth-dare-generator", requestOptions)
-let resJSON = await res.json()
-let truth = resJSON.text
         const embed = new EmbedBuilder()
             .setTitle(`${interaction.user.username} Dared:`)
             .setDescription(`<@${user.id}>, ${truth}`)
@@ -39,7 +40,7 @@ let truth = resJSON.text
             });
 
         interaction.reply({
-            embeds: [embed]
-        })
-    }
-}
+            embeds: [embed],
+        });
+    },
+};
